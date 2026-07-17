d04084dcd29108f0260693019927a0596a7b287f
vinivi85 <vinivi85@hotmail.com>
2026-07-17 15:55:45 -0500

    Gemini: flash-8b + retry 3x em 429 (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v65';
-const APP_VERSION = 'v65';
-const BUILD_DATE = '2026-07-17 03:56 CDT';
+const CACHE_NAME = '401k-tracker-v66';
+const APP_VERSION = 'v66';
+const BUILD_DATE = '2026-07-17 15:55 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
