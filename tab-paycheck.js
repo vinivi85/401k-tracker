fbd21e1831bf85b28bb40ae69d09cddd9389d74f
vinivi85 <vinivi85@hotmail.com>
2026-07-17 19:13:11 -0500

    Gemini: atualiza para 3.5-flash (tab-paycheck.js)

--- tab-paycheck.js
+++ tab-paycheck.js
@@ -7,7 +7,7 @@
   var h = React.createElement;
 
   var GEMINI_API_KEY = window.__GEMINI_KEY || '';
-  var GEMINI_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=' + GEMINI_API_KEY;
+  var GEMINI_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent?key=' + GEMINI_API_KEY;
 
   /* ---------- Cálculo do contracheque ---------- */
   function calcPaycheck(cfg) {
