a6356f97be76543f1ee31648a942cfff1c0d3a29
vinivi85 <vinivi85@hotmail.com>
2026-07-17 22:42:33 -0500

    Fix: retry em 503 alem de 429 (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v79';
-const APP_VERSION = 'v79';
-const BUILD_DATE = '2026-07-17 22:16 CDT';
+const CACHE_NAME = '401k-tracker-v80';
+const APP_VERSION = 'v80';
+const BUILD_DATE = '2026-07-17 22:42 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
