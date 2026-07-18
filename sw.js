d6e019d1527bf7dc370e0c6f89741cd090284ade
vinivi85 <vinivi85@hotmail.com>
2026-07-18 16:20:28 -0500

    Import imagem Work Summary: Gemini Vision extrai horas REG/OT1.5/OT2.0 (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v86';
-const APP_VERSION = 'v86';
-const BUILD_DATE = '2026-07-18 12:26 CDT';
+const CACHE_NAME = '401k-tracker-v87';
+const APP_VERSION = 'v87';
+const BUILD_DATE = '2026-07-18 16:20 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
