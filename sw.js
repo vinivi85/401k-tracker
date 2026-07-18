f44622532968250e571b45bd4412249613ea27c0
vinivi85 <vinivi85@hotmail.com>
2026-07-17 21:11:46 -0500

    Paycheck: adiciona campos SICK e VACATION (taxa base, Shift2 diff incluso) (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v72';
-const APP_VERSION = 'v72';
-const BUILD_DATE = '2026-07-17 20:59 CDT';
+const CACHE_NAME = '401k-tracker-v73';
+const APP_VERSION = 'v73';
+const BUILD_DATE = '2026-07-17 21:11 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
