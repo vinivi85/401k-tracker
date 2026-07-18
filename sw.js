23da74a8198f36e71eaea37e681dc0d9bc084732
vinivi85 <vinivi85@hotmail.com>
2026-07-18 12:26:40 -0500

    Fix: duplicidade detecta correto, 401k usa valores do PDF, opcao atualizar (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v85';
-const APP_VERSION = 'v85';
-const BUILD_DATE = '2026-07-18 10:06 CDT';
+const CACHE_NAME = '401k-tracker-v86';
+const APP_VERSION = 'v86';
+const BUILD_DATE = '2026-07-18 12:26 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
