bee300b1572cd55239feca9d719a23846ec9272f
vinivi85 <vinivi85@hotmail.com>
2026-07-18 18:54:41 -0500

    Tracker: variacao ultimo mes vs desde o inicio no card 401k (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v88';
-const APP_VERSION = 'v88';
-const BUILD_DATE = '2026-07-18 18:49 CDT';
+const CACHE_NAME = '401k-tracker-v89';
+const APP_VERSION = 'v89';
+const BUILD_DATE = '2026-07-18 18:54 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
