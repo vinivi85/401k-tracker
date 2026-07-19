07f4fa2a78d2efde267a47b9c0e9058cf33e6148
vinivi85 <vinivi85@hotmail.com>
2026-07-18 22:56:21 -0500

    Fix Storage: path encoding correto uid/filename sem duplicar uid (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v93';
-const APP_VERSION = 'v93';
-const BUILD_DATE = '2026-07-18 22:46 CDT';
+const CACHE_NAME = '401k-tracker-v94';
+const APP_VERSION = 'v94';
+const BUILD_DATE = '2026-07-18 22:56 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
