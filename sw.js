10d965f8352b7e8b3aacf62d7d1447f798ddc19e
vinivi85 <vinivi85@hotmail.com>
2026-07-19 23:25:31 -0500

    Fix: remove setInterval e listeners de atividade, nao bloqueia mais por inatividade (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = '401k-tracker-v94';
 const APP_VERSION = 'v94';
-const BUILD_DATE = '2026-07-19 22:01 CDT';
+const BUILD_DATE = '2026-07-19 23:25 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
