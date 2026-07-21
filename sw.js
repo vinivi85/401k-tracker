0e86a3847e56ee225e9f80bd1081779e2f150428
vinivi85 <vinivi85@hotmail.com>
2026-07-21 00:13:57 -0500

    PDF Viewer: abre no Safari nativo via window.open com URL assinada (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v129';
-const APP_VERSION = 'v129';
-const BUILD_DATE = '2026-07-21 00:07 CDT';
+const CACHE_NAME = '401k-tracker-v130';
+const APP_VERSION = 'v130';
+const BUILD_DATE = '2026-07-21 00:13 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
