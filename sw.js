bfa1980b9b0f3cea105428f32fd19af1dcced963
vinivi85 <vinivi85@hotmail.com>
2026-07-21 23:37:12 -0500

    Config: selecionar faixa salarial atualiza taxa base automaticamente (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v140';
-const APP_VERSION = 'v140';
-const BUILD_DATE = '2026-07-21 02:02 CDT';
+const CACHE_NAME = '401k-tracker-v141';
+const APP_VERSION = 'v141';
+const BUILD_DATE = '2026-07-21 23:37 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
