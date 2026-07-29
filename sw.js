5d06e39b627843b33ecd74bd801d2260265317ae
vinivi85 <vinivi85@hotmail.com>
2026-07-28 23:27:20 -0500

    IRS wizard completo 6 etapas (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v145';
-const APP_VERSION = 'v145';
-const BUILD_DATE = '2026-07-28 13:09 CDT';
+const CACHE_NAME = '401k-tracker-v146';
+const APP_VERSION = 'v146';
+const BUILD_DATE = '2026-07-28 23:27 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
