c41614220f6097e180d9556fb1be3d9cf78aaea6
vinivi85 <vinivi85@hotmail.com>
2026-08-04 23:19:54 -0500

    Fix: ano 2025 na pergunta filing status (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v149';
-const APP_VERSION = 'v149';
-const BUILD_DATE = '2026-08-04 11:39 CDT';
+const CACHE_NAME = '401k-tracker-v150';
+const APP_VERSION = 'v150';
+const BUILD_DATE = '2026-08-04 23:19 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
