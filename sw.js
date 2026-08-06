411d65e393793195a216d46eb744353882880713
vinivi85 <vinivi85@hotmail.com>
2026-08-06 13:27:02 -0500

    Import PDF: extrai aliquotas federais e atualiza CONFIG automaticamente (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v153';
-const APP_VERSION = 'v153';
-const BUILD_DATE = '2026-08-06 13:15 CDT';
+const CACHE_NAME = '401k-tracker-v154';
+const APP_VERSION = 'v154';
+const BUILD_DATE = '2026-08-06 13:26 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
