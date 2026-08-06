9dd6903d27a88dbd22e579a2977edde1c2a0f6d8
vinivi85 <vinivi85@hotmail.com>
2026-08-06 14:50:57 -0500

    Config: fedWithholdingPct 1.83% extraido do PDF 08/07/2026 (core.js)

--- core.js
+++ core.js
@@ -33,7 +33,7 @@ var h = React.createElement;
     profitSharingPct: 5,         // 401K AA Contrib — contribuição automática AA (% editável)
     ssRatePct: 6.2,
     medicareRatePct: 1.45,
-    fedWithholdingPct: 1.2316,  // efetivo aproximado, editável (varia bastante com o gross)
+    fedWithholdingPct: 1.83,   // extraído do PDF 08/07/2026  // efetivo aproximado, editável (varia bastante com o gross)
     regHours: 80,
     sickHours: 0,
     vacationHours: 0,
