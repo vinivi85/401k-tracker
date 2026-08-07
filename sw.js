f596e9c9e7ee3da65c5009909bcf4a1180b9a9cb
vinivi85 <vinivi85@hotmail.com>
2026-08-07 10:05:39 -0500

    IRS: campo federal withheld conjuge na etapa renda (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v159';
-const APP_VERSION = 'v159';
-const BUILD_DATE = '2026-08-06 17:20 CDT';
+const CACHE_NAME = '401k-tracker-v160';
+const APP_VERSION = 'v160';
+const BUILD_DATE = '2026-08-07 10:05 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
