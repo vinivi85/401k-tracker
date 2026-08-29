34443155a8fd8f45622ab6ed23060d2c84d3cf95
vinivi85 <vinivi85@hotmail.com>
2026-08-29 11:54:25 -0500

    Fix tela preta: deploy limpo (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v181';
-const APP_VERSION = 'v181';
-const BUILD_DATE = '2026-08-29 11:50 CDT';
+const CACHE_NAME = '401k-tracker-v182';
+const APP_VERSION = 'v182';
+const BUILD_DATE = '2026-08-29 11:54 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
