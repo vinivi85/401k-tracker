7123fd9c7e17993adcad13c157d5074aa09e7d61
vinivi85 <vinivi85@hotmail.com>
2026-08-28 20:18:20 -0500

    Fix Work Summary: OTS->additionalHours, LUNCH-P->lunchHours, sem OT2.0 ok (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v165';
-const APP_VERSION = 'v165';
-const BUILD_DATE = '2026-08-28 19:21 CDT';
+const CACHE_NAME = '401k-tracker-v166';
+const APP_VERSION = 'v166';
+const BUILD_DATE = '2026-08-28 20:18 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
