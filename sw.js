9b72ba828ccdffe3bd41289fa9f16718485214b7
vinivi85 <vinivi85@hotmail.com>
2026-08-29 12:07:24 -0500

    Tracker: botao +LEITURA abaixo do header, estilo smallAddBtn (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v173';
-const APP_VERSION = 'v173';
-const BUILD_DATE = '2026-08-29 08:42 CDT';
+const CACHE_NAME = '401k-tracker-v174';
+const APP_VERSION = 'v174';
+const BUILD_DATE = '2026-08-29 12:07 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
