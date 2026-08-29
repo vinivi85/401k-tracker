1f712e595bd939f23820df06d2c98e7a134703dc
vinivi85 <vinivi85@hotmail.com>
2026-08-28 20:30:57 -0500

    Fix: OTS soma no regHours, nao em additionalHours (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v170';
-const APP_VERSION = 'v170';
-const BUILD_DATE = '2026-08-28 20:29 CDT';
+const CACHE_NAME = '401k-tracker-v171';
+const APP_VERSION = 'v171';
+const BUILD_DATE = '2026-08-28 20:30 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
