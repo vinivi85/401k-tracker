4a1e66ee36862e19204afd0cc4b0b10222a80f54
vinivi85 <vinivi85@hotmail.com>
2026-08-31 13:30:48 -0500

    Plaid: botao desconectar, check duplicata, badge plaid no header (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v178';
-const APP_VERSION = 'v178';
-const BUILD_DATE = '2026-08-31 13:26 CDT';
+const CACHE_NAME = '401k-tracker-v179';
+const APP_VERSION = 'v179';
+const BUILD_DATE = '2026-08-31 13:30 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
