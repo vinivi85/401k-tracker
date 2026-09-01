d43883349ab39309a18a4d6b16d58982a9c00194
vinivi85 <vinivi85@hotmail.com>
2026-09-01 12:30:45 -0500

    Fix: 401k sync nao grava se valor igual, fix entry_date (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v202';
-const APP_VERSION = 'v202';
-const BUILD_DATE = '2026-09-01 12:24 CDT';
+const CACHE_NAME = '401k-tracker-v203';
+const APP_VERSION = 'v203';
+const BUILD_DATE = '2026-09-01 12:30 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
