ba77d6880a77bcdec37daef47f88de9fc215a5a3
vinivi85 <vinivi85@hotmail.com>
2026-09-01 15:52:02 -0500

    Config: nome das contas somente via Tracker, sem edicao manual (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v205';
-const APP_VERSION = 'v205';
-const BUILD_DATE = '2026-09-01 15:48 CDT';
+const CACHE_NAME = '401k-tracker-v206';
+const APP_VERSION = 'v206';
+const BUILD_DATE = '2026-09-01 15:52 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
