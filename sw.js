e0767e8ff8bca4ae675e2287d207f4ec511eb786
vinivi85 <vinivi85@hotmail.com>
2026-08-31 22:49:12 -0500

    Tracker: mostra data/hora ultima atualizacao no card 401K (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v187';
-const APP_VERSION = 'v187';
-const BUILD_DATE = '2026-08-31 22:40 CDT';
+const CACHE_NAME = '401k-tracker-v188';
+const APP_VERSION = 'v188';
+const BUILD_DATE = '2026-08-31 22:49 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
