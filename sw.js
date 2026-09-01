61a72eec880fe195609a1b4cc1d7bf61e5e76c6e
vinivi85 <vinivi85@hotmail.com>
2026-09-01 15:55:43 -0500

    Tracker: botao EDITAR nome carteira, CONTA no lugar de CARTEIRA (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v206';
-const APP_VERSION = 'v206';
-const BUILD_DATE = '2026-09-01 15:52 CDT';
+const CACHE_NAME = '401k-tracker-v207';
+const APP_VERSION = 'v207';
+const BUILD_DATE = '2026-09-01 15:55 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
