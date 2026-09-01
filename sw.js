d10a2b035c39dd52db698e434192e41e22247d45
vinivi85 <vinivi85@hotmail.com>
2026-08-31 19:34:32 -0500

    Plaid sync: botao CONFIG e Tracker, cron diario 9am CDT (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v182';
-const APP_VERSION = 'v182';
-const BUILD_DATE = '2026-08-31 14:18 CDT';
+const CACHE_NAME = '401k-tracker-v183';
+const APP_VERSION = 'v183';
+const BUILD_DATE = '2026-08-31 19:34 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
