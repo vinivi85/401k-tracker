5dfed0cfb8ee4163b2f5884c3bf96d85b013b16c
vinivi85 <vinivi85@hotmail.com>
2026-08-31 22:40:28 -0500

    Fix: associar mostra carteiras do Tracker (Supabase) nao fundos do CONFIG (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v186';
-const APP_VERSION = 'v186';
-const BUILD_DATE = '2026-08-31 22:37 CDT';
+const CACHE_NAME = '401k-tracker-v187';
+const APP_VERSION = 'v187';
+const BUILD_DATE = '2026-08-31 22:40 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
