1739dda196a66ec5e07aff0982876257dcbe57c4
vinivi85 <vinivi85@hotmail.com>
2026-09-01 10:57:48 -0500

    Fix: sync mostra msg criado/atualizado/erro e log detalhado (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v192';
-const APP_VERSION = 'v192';
-const BUILD_DATE = '2026-09-01 08:35 CDT';
+const CACHE_NAME = '401k-tracker-v193';
+const APP_VERSION = 'v193';
+const BUILD_DATE = '2026-09-01 10:57 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
