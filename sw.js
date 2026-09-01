7355113fcdf6b8f97e19da8357092d90ebe687cc
vinivi85 <vinivi85@hotmail.com>
2026-09-01 16:14:17 -0500

    Fix: remove estados duplicados do WalletCard (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v211';
-const APP_VERSION = 'v211';
-const BUILD_DATE = '2026-09-01 16:11 CDT';
+const CACHE_NAME = '401k-tracker-v212';
+const APP_VERSION = 'v212';
+const BUILD_DATE = '2026-09-01 16:14 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
