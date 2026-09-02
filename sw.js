6f325f451b5a302ac763f0053d7cd5d97783e664
vinivi85 <vinivi85@hotmail.com>
2026-09-02 00:35:36 -0500

    Fix: optimistic rename, reverte se falhar (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v216';
-const APP_VERSION = 'v216';
-const BUILD_DATE = '2026-09-01 19:17 CDT';
+const CACHE_NAME = '401k-tracker-v217';
+const APP_VERSION = 'v217';
+const BUILD_DATE = '2026-09-02 00:35 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
