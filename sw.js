b39e0158bf1baf5557058b9fc2791c2c9fb5297c
vinivi85 <vinivi85@hotmail.com>
2026-09-01 19:17:58 -0500

    Fix updateWallet: simplifica PATCH sem encodeURI (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v215';
-const APP_VERSION = 'v215';
-const BUILD_DATE = '2026-09-01 19:14 CDT';
+const CACHE_NAME = '401k-tracker-v216';
+const APP_VERSION = 'v216';
+const BUILD_DATE = '2026-09-01 19:17 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
