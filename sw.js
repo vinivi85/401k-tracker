1166b0f3ccccc9f847183387a21542ec70f061a4
vinivi85 <vinivi85@hotmail.com>
2026-09-02 00:55:02 -0500

    Config: IMPORTAR CONTA busca carteiras do Tracker (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v218';
-const APP_VERSION = 'v218';
-const BUILD_DATE = '2026-09-02 00:52 CDT';
+const CACHE_NAME = '401k-tracker-v219';
+const APP_VERSION = 'v219';
+const BUILD_DATE = '2026-09-02 00:55 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
