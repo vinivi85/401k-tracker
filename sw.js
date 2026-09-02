a9a793ba9f27104643d595139b2b21bd4ba7a97f
vinivi85 <vinivi85@hotmail.com>
2026-09-01 19:11:55 -0500

    Add updateWallet to supabase-client (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v213';
-const APP_VERSION = 'v213';
-const BUILD_DATE = '2026-09-01 16:30 CDT';
+const CACHE_NAME = '401k-tracker-v214';
+const APP_VERSION = 'v214';
+const BUILD_DATE = '2026-09-01 19:11 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
