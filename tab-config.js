6b5870d55d4d7f8d1a304fa1d2de332a8ba71a80
vinivi85 <vinivi85@hotmail.com>
2026-09-02 15:57:33 -0500

    Google Drive: auth no CONFIG, picker PDF no PAYCHECK (tab-config.js)

--- tab-config.js
+++ tab-config.js
@@ -994,6 +994,9 @@
         })
       ),
 
+      /* ---- GOOGLE DRIVE ---- */
+      h(GoogleDriveSection, { userId: window.currentUserId ? window.currentUserId() : null, cfg: cfg, update: update }),
+
       /* ---- RESET GERAL ---- */
       h('div', { style: S.card },
         h('div', { style: S.cardHeader }, h('span', { style: S.cardTitle }, 'RESTAURAR TUDO')),
