d4e3c10bec43ef83a3dc92d9b363d968879cc7a1
vinivi85 <vinivi85@hotmail.com>
2026-09-02 00:52:59 -0500

    Rename: dispara evento, CONFIG recarrega wallets automaticamente (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v217';
-const APP_VERSION = 'v217';
-const BUILD_DATE = '2026-09-02 00:35 CDT';
+const CACHE_NAME = '401k-tracker-v218';
+const APP_VERSION = 'v218';
+const BUILD_DATE = '2026-09-02 00:52 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
