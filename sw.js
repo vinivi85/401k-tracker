5b327e88a2919a1652d2a6825220a981d352e445
vinivi85 <vinivi85@hotmail.com>
2026-09-02 22:19:53 -0500

    Fix: loadEntries undefined, secao aposentadoria com pre-compute (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v233';
-const APP_VERSION = 'v233';
-const BUILD_DATE = '2026-09-02 22:18 CDT';
+const CACHE_NAME = '401k-tracker-v234';
+const APP_VERSION = 'v234';
+const BUILD_DATE = '2026-09-02 22:19 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
