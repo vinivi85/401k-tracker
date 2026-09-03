5949714edb170e85ae57cac73b4e7878051b5cdf
vinivi85 <vinivi85@hotmail.com>
2026-09-02 23:41:38 -0500

    Fix: botao RECONECTAR quando offline (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v238';
-const APP_VERSION = 'v238';
-const BUILD_DATE = '2026-09-02 23:30 CDT';
+const CACHE_NAME = '401k-tracker-v239';
+const APP_VERSION = 'v239';
+const BUILD_DATE = '2026-09-02 23:41 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
