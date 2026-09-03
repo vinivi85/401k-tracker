bedbd4c19ee0cc60807119a62f356249bb2dedb8
vinivi85 <vinivi85@hotmail.com>
2026-09-02 22:03:26 -0500

    Parte 1: secao CONTAS DE APOSENTADORIA, renomeia label (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v227';
-const APP_VERSION = 'v227';
-const BUILD_DATE = '2026-09-02 21:56 CDT';
+const CACHE_NAME = '401k-tracker-v228';
+const APP_VERSION = 'v228';
+const BUILD_DATE = '2026-09-02 22:03 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
