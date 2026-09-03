05337c24736a6368c52a79afa17944d94746bb5d
vinivi85 <vinivi85@hotmail.com>
2026-09-02 23:25:00 -0500

    Clean rebuild: secoes aposentadoria+investimento (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v235';
-const APP_VERSION = 'v235';
-const BUILD_DATE = '2026-09-02 22:26 CDT';
+const CACHE_NAME = '401k-tracker-v236';
+const APP_VERSION = 'v236';
+const BUILD_DATE = '2026-09-02 23:24 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
