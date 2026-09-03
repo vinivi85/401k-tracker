b22593183f20506fa62e99058590eb60d9446cd1
vinivi85 <vinivi85@hotmail.com>
2026-09-02 22:15:26 -0500

    Fix: retirementCards sem Fragment, usa ternario (tab-tracker.js)

--- tab-tracker.js
+++ tab-tracker.js
@@ -609,7 +609,7 @@
       ),
 
       /* Retirement wallet cards */
-      h(React.Fragment, null, retirementCards.map(function(wc) {
+      retirementCards.length > 0 ? retirementCards.map(function(wc) {
         return h(WalletCard, {
           key: wc.wallet.id,
           wallet: wc.wallet,
@@ -619,7 +619,7 @@
           onDeleteWallet: handleDeleteWallet,
           onRenameWallet: handleRenameWallet
         });
-      })),
+      }) : null,
 
       h(WalletsSection, {
         wallets: wallets,
