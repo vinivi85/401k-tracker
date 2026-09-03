fa679d3e7fe23f1fe6b2490ac2e2c7d972c55afe
vinivi85 <vinivi85@hotmail.com>
2026-09-02 22:05:57 -0500

    Parte 2: Fidelity 401k-AA na secao aposentadoria (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v228';
-const APP_VERSION = 'v228';
-const BUILD_DATE = '2026-09-02 22:03 CDT';
+const CACHE_NAME = '401k-tracker-v229';
+const APP_VERSION = 'v229';
+const BUILD_DATE = '2026-09-02 22:05 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
