341c534cac61bd1753924689eb8fab367a2b0a85
vinivi85 <vinivi85@hotmail.com>
2026-09-02 21:44:29 -0500

    Fix: Drive import usa funcoes corretas do paycheck (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v225';
-const APP_VERSION = 'v225';
-const BUILD_DATE = '2026-09-02 15:57 CDT';
+const CACHE_NAME = '401k-tracker-v226';
+const APP_VERSION = 'v226';
+const BUILD_DATE = '2026-09-02 21:44 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
