a1d0750b152312fc104e918670df17dff769fd4c
vinivi85 <vinivi85@hotmail.com>
2026-09-02 22:15:27 -0500

    Fix: retirementCards sem Fragment, usa ternario (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v231';
-const APP_VERSION = 'v231';
-const BUILD_DATE = '2026-09-02 22:12 CDT';
+const CACHE_NAME = '401k-tracker-v232';
+const APP_VERSION = 'v232';
+const BUILD_DATE = '2026-09-02 22:15 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
