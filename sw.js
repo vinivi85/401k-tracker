6dc01615623614ae604452857e3c915fcc36b8b4
vinivi85 <vinivi85@hotmail.com>
2026-09-04 15:35:19 -0500

    Tracker: mensagem de sync por conta no card (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v253';
-const APP_VERSION = 'v253';
-const BUILD_DATE = '2026-09-04 15:26 CDT';
+const CACHE_NAME = '401k-tracker-v254';
+const APP_VERSION = 'v254';
+const BUILD_DATE = '2026-09-04 15:35 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
