2fe4071f94088e901383c6e709645dcf6f034d2c
vinivi85 <vinivi85@hotmail.com>
2026-09-04 02:03:22 -0500

    Item 1: engrenagem config no header, remove OFFLINE-READY (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v243';
-const APP_VERSION = 'v243';
-const BUILD_DATE = '2026-09-04 02:00 CDT';
+const CACHE_NAME = '401k-tracker-v244';
+const APP_VERSION = 'v244';
+const BUILD_DATE = '2026-09-04 02:03 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
