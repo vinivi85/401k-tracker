d3ed502132a79901f7d0ffed0c583e3db845e7e9
vinivi85 <vinivi85@hotmail.com>
2026-09-05 23:20:54 -0500

    v276

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = '401k-tracker-v275';
-const APP_VERSION = 'v275';
-const BUILD_DATE = '2026-09-05 23:14 CDT';
+const CACHE_NAME = '401k-tracker-v276';
+const APP_VERSION = 'v276';
+const BUILD_DATE = '2026-09-05 23:20 CDT';
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
