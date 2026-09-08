a5b285436363644020dd9dd3f627438199fce68c
vinivi85 <vinivi85@hotmail.com>
2026-09-08 18:16:13 -0500

    Storage: chave para persistir data do ultimo sync Plaid

--- core.js
+++ core.js
@@ -16,6 +16,7 @@ var h = React.createElement;
   var KEY_WALLETS = '401k-wallets';
   var KEY_WALLET_ENTRIES = '401k-wallet-entries';
   var KEY_SYNC_MSGS = '401k-sync-msgs';
+  var KEY_LAST_PLAID_SYNC = '401k-last-plaid-sync';
   var KEY_LOCK_CONFIG = '401k-lock-config';
   var KEY_AUTH_SESSION = '401k-auth-session';
 
@@ -164,7 +165,7 @@ var h = React.createElement;
   // Carrega TODOS os dados do IndexedDB para o cache em memória.
   // Também migra qualquer dado remanescente do localStorage (de versões antigas do app)
   // para o IndexedDB, então funciona como upgrade transparente.
-  var ALL_KEYS = [KEY_ENTRIES, KEY_PAYCHECK, KEY_PROJECTION, KEY_ACTIVE_TAB, KEY_PAY_ENTRIES, KEY_WALLETS, KEY_WALLET_ENTRIES, KEY_LOCK_CONFIG, KEY_AUTH_SESSION, KEY_SYNC_MSGS];
+  var ALL_KEYS = [KEY_ENTRIES, KEY_PAYCHECK, KEY_PROJECTION, KEY_ACTIVE_TAB, KEY_PAY_ENTRIES, KEY_WALLETS, KEY_WALLET_ENTRIES, KEY_LOCK_CONFIG, KEY_AUTH_SESSION, KEY_SYNC_MSGS, KEY_LAST_PLAID_SYNC];
 
   function initStorage() {
     return openDB().then(function () {
